2db788996dbc9105e43eaa08289d7e80eb19b2d6
kiwimiho <summer.shen.2007@gmail.com>
2023-12-09 15:20:20 -0800

    questinoable db thing (will never be inplimented)

--- backend.py
+++ backend.py
@@ -1,11 +1,32 @@
 import flask
+import sqlite3
 
+conn = sqlite3.connect('tutoring.db')
+cursor = conn.cursor()
 
 app = flask.Flask("berrynperry")
+
+user_db = {
+    "Summer": {
+        "subject":"CS",
+        "Bio":"I am beary",
+        "contact":{
+            "number":"4084084081",
+            "discord":"ihatekiwis",
+            "email":"pooh@gmail.com"
+        }
+    }
+}
 
 @app.route("/")
 def main():
     return "lol"
 
+@app.route("/lookup",methods=["POST"])
+
+def lookup(subject):
+    cursor.execute('SELECT * FROM users WHERE bio LIKE ?', ('%' + subject + '%',))
+    results = cursor.fetchall()
+    return results
 
 app.run('0.0.0.0',port=8080)
---
+++ test.py
@@ -0,0 +1,7 @@
+berrys_backpack = {
+    "items":"car",
+    "pocket":"tree"
+}
+
+berrys_backpack["trunk"] = "perry"
+print(berrys_backpack)
